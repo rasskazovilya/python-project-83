25e2b15f6ca05c61bfdb63e8fc671513b5f5cb96
Ilya <rasskazov_ilya_98@mail.ru>
2023-11-20 14:52:18 +0300

    add route for posting new checks

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -48,7 +48,7 @@ def add_site():
         return render_template('index.html', code=422)
 
     parsed_url = urlparse(url)
-    root_url = f'{parsed_url.scheme}:{parsed_url.netloc}'
+    root_url = f'{parsed_url.scheme}://{parsed_url.netloc}'
 
     with conn.cursor() as curs:
         created_at = datetime.datetime.today().replace(microsecond=0)
@@ -70,9 +70,39 @@ def get_site(id):
         select_id_query = "SELECT * FROM urls WHERE id=%s;"
         curs.execute(select_id_query, (id,))
         url = curs.fetchone()
-    if request.method == "POST":
-        flash('Страница успешно добавлена', 'success')
+
+        select_checks_query = """
+        SELECT * FROM url_checks
+        WHERE url_id=%s
+        ORDER BY created_at DESC;
+        """
+        curs.execute(select_checks_query, (id,))
+        checks = curs.fetchall()
+
     return render_template(
         'url.html',
-        url=url
+        url=url,
+        checks=checks
     )
+
+
+@app.post("/urls/<id>/checks")
+def check_site(id):
+    with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curs:
+        created_at = datetime.datetime.today().replace(microsecond=0)
+
+        add_check = """
+        INSERT INTO url_checks (
+            url_id,
+            status_code,
+            h1,
+            title,
+            description,
+            created_at
+        ) VALUES (%s, %s, %s, %s, %s, %s);
+        """
+        curs.execute(add_check, (id, 200, '', '', '', created_at))
+        conn.commit()
+
+    flash('Страница успешно проверена', 'success')
+    return redirect(url_for("get_site", id=id), code=302)

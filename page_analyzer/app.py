c222bf8b90d888402516cec2e075f62e61c1d1c4
Ilya <rasskazov_ilya_98@mail.ru>
2023-11-23 15:37:34 +0300

    move connecting to db in db.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,9 +1,8 @@
 import os
 from urllib.parse import urlparse
 
-import psycopg2
-import requests
 import bs4
+import requests
 from dotenv import load_dotenv
 from flask import Flask, flash, redirect, render_template, request, url_for
 from page_analyzer import db
@@ -11,28 +10,22 @@ from page_analyzer import db
 from .url_validator import validate
 
 load_dotenv()
-DATABASE_URL = os.getenv('DATABASE_URL')
+
 
 app = Flask(__name__)
-app.secret_key = os.getenv('SECRET_KEY')
+app.secret_key = os.getenv("SECRET_KEY")
 
 
 @app.route("/")
 def index():
-    return render_template(
-        'index.html'
-    )
+    return render_template("index.html")
 
 
 @app.get("/urls")
 def get_urls():
-    with psycopg2.connect(DATABASE_URL) as conn:
-        urls = db.get_urls(conn)
+    urls = db.get_urls()
 
-    return render_template(
-        'url_table.html',
-        urls=urls
-    )
+    return render_template("url_table.html", urls=urls)
 
 
 @app.post("/urls")
@@ -42,66 +35,51 @@ def add_url():
     errors = validate(url)
     if errors:
         for error in errors:
-            flash(error, 'danger')
-        return render_template('index.html'), 422
+            flash(error, "danger")
+        return render_template("index.html"), 422
 
     parsed_url = urlparse(url)
-    root_url = f'{parsed_url.scheme}://{parsed_url.netloc}'
+    root_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
 
-    with psycopg2.connect(DATABASE_URL) as conn:
-        exist_url = db.get_url(conn, cond='name', value=root_url)
+    exist_url = db.get_url(cond="name", value=root_url)
     if exist_url:
-        flash('Страница уже существует', 'info')
-        return redirect(url_for("get_url", id=exist_url['id']), code=302)
+        flash("Страница уже существует", "info")
+        return redirect(url_for("get_url", id=exist_url["id"]), code=302)
 
-    with psycopg2.connect(DATABASE_URL) as conn:
-        id = db.add_url(conn, root_url)
-    flash('Страница успешно добавлена', 'success')
+    id = db.add_url(root_url)
+    flash("Страница успешно добавлена", "success")
     return redirect(url_for("get_url", id=id), code=302)
 
 
 @app.get("/urls/<id>")
 def get_url(id):
-    with psycopg2.connect(DATABASE_URL) as conn:
-        url = db.get_url(conn, cond='id', value=id)
-        checks = db.get_url_checks(conn, id)
 
-    return render_template(
-        'url.html',
-        url=url,
-        checks=checks
-    )
+    url = db.get_url(cond="id", value=id)
+    checks = db.get_url_checks(id)
+
+    return render_template("url.html", url=url, checks=checks)
 
 
 @app.post("/urls/<id>/checks")
 def check_url(id):
-    with psycopg2.connect(DATABASE_URL) as conn:
-        url = db.get_url(conn, cond='id', value=id)
-        print(url)
+
+    url = db.get_url(cond="id", value=id)
     try:
-        response = requests.get(url['name'])
+        response = requests.get(url["name"])
         response.raise_for_status()
         if response.status_code != 200:
             raise requests.RequestException
     except requests.RequestException:
-        flash('Произошла ошибка при проверке', 'danger')
+        flash("Произошла ошибка при проверке", "danger")
         return redirect(url_for("get_url", id=id))
 
-    bs = bs4.BeautifulSoup(response.text, 'html.parser')
-    h1 = bs.h1.string if bs.h1 else ''
-    title = bs.title.string if bs.title else ''
-    desc_tag = bs.find('meta', attrs={'name': 'description'})
-    desc = desc_tag.get('content') if desc_tag else ''
+    bs = bs4.BeautifulSoup(response.text, "html.parser")
+    h1 = bs.h1.string if bs.h1 else ""
+    title = bs.title.string if bs.title else ""
+    desc_tag = bs.find("meta", attrs={"name": "description"})
+    desc = desc_tag.get("content") if desc_tag else ""
 
-    with psycopg2.connect(DATABASE_URL) as conn:
-        db.add_check(
-            conn,
-            id,
-            response.status_code,
-            h1=h1,
-            title=title,
-            desc=desc
-        )
+    db.add_check(id, response.status_code, h1, title, desc)
 
-    flash('Страница успешно проверена', 'success')
+    flash("Страница успешно проверена", "success")
     return redirect(url_for("get_url", id=id))
--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -1,79 +1,87 @@
+import datetime
+import os
+
 import psycopg2
 import psycopg2.extras
-import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 
-def get_urls(conn):
-    with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curs:
-        select_all_query = """
-        SELECT urls.id,
-            name,
-            MAX(url_checks.created_at) AS last_date,
-            status_code
-        FROM urls
-        LEFT JOIN url_checks ON urls.id = url_checks.url_id
-        GROUP BY urls.id, status_code
-        ORDER BY urls.id DESC;
-        """
-        curs.execute(select_all_query)
-        urls = curs.fetchall()
+def get_urls():
+    with psycopg2.connect(DATABASE_URL) as conn:
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curs:
+            select_all_query = """
+            SELECT urls.id,
+                name,
+                MAX(url_checks.created_at) AS last_date,
+                status_code
+            FROM urls
+            LEFT JOIN url_checks ON urls.id = url_checks.url_id
+            GROUP BY urls.id, status_code
+            ORDER BY urls.id DESC;
+            """
+            curs.execute(select_all_query)
+            urls = curs.fetchall()
     return urls
 
 
-def add_url(conn, url):
-    with conn.cursor() as curs:
-        created_at = datetime.datetime.today().replace(microsecond=0)
-        insert_query = """
-        INSERT INTO urls (name, created_at)
-        VALUES (%s, %s) RETURNING id;
-        """
-        curs.execute(insert_query, (url, created_at))
-        conn.commit()
-        id = curs.fetchone()[0]
+def add_url(url):
+    with psycopg2.connect(DATABASE_URL) as conn:
+        with conn.cursor() as curs:
+            created_at = datetime.datetime.today().replace(microsecond=0)
+            insert_query = """
+            INSERT INTO urls (name, created_at)
+            VALUES (%s, %s) RETURNING id;
+            """
+            curs.execute(insert_query, (url, created_at))
+            conn.commit()
+            id = curs.fetchone()[0]
     return id
 
 
-def get_url(conn, cond, value):
-    conditions = {
-        'name': 'WHERE name=%s;',
-        'id': 'WHERE id=%s;'
-    }
-
-    with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curs:
-        select_name_query = "SELECT * FROM urls " + conditions[cond]
-        curs.execute(select_name_query, (value,))
-        url = curs.fetchone()
+def get_url(cond, value):
+    conditions = {"name": "WHERE name=%s;", "id": "WHERE id=%s;"}
+    with psycopg2.connect(DATABASE_URL) as conn:
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curs:
+            select_name_query = "SELECT * FROM urls " + conditions[cond]
+            curs.execute(select_name_query, (value,))
+            url = curs.fetchone()
     return url
 
 
-def get_url_checks(conn, url_id):
-    with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curs:
-        select_checks_query = """
-        SELECT * FROM url_checks
-        WHERE url_id=%s
-        ORDER BY created_at DESC;
-        """
-        curs.execute(select_checks_query, (url_id,))
-        checks = curs.fetchall()
+def get_url_checks(url_id):
+    with psycopg2.connect(DATABASE_URL) as conn:
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curs:
+            select_checks_query = """
+            SELECT * FROM url_checks
+            WHERE url_id=%s
+            ORDER BY created_at DESC;
+            """
+            curs.execute(select_checks_query, (url_id,))
+            checks = curs.fetchall()
     return checks
 
 
-def add_check(conn, url_id, status_code, h1, title, desc):
-    with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curs:
-        created_at = datetime.datetime.today().replace(microsecond=0)
+def add_check(url_id, status_code, h1, title, desc):
+    with psycopg2.connect(DATABASE_URL) as conn:
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curs:
+            created_at = datetime.datetime.today().replace(microsecond=0)
 
-        add_check_query = """
-        INSERT INTO url_checks (
-            url_id,
-            status_code,
-            h1,
-            title,
-            description,
-            created_at
-        ) VALUES (%s, %s, %s, %s, %s, %s);
-        """
-        curs.execute(
-            add_check_query,
-            (url_id, status_code, h1, title, desc, created_at)
-        )
-        conn.commit()
+            add_check_query = """
+            INSERT INTO url_checks (
+                url_id,
+                status_code,
+                h1,
+                title,
+                description,
+                created_at
+            ) VALUES (%s, %s, %s, %s, %s, %s);
+            """
+            curs.execute(
+                add_check_query,
+                (url_id, status_code, h1, title, desc, created_at)
+            )
+            conn.commit()
+    return
